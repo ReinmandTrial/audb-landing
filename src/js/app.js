a82432b5b002c86e1a5fa8e2472ccdbf302be6c3
Nik Horbachov <nikcosmos@gmail.com>
2023-09-04 19:44:50 +0300

    feature: add smooth scroll

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,12 @@
 import '../scss/style.scss';
 import './modules.js';
 import './watcher.js';
+import '/node_modules/locomotive-scroll/dist/locomotive-scroll.min.css';
+
+import LocomotiveScroll from 'locomotive-scroll';
+
+const scroll = new LocomotiveScroll({
+   el: document.querySelector('[data-scroll-container]'),
+   smooth: true,
+   lerp: 0.07,
+});
